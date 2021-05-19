c7bb23c3203539fdeb12494ab271c4df0c52ead6
guoyan <44565096+boy-yan@users.noreply.github.com>
2021-05-19 17:40:25 +0800

    feat: 优化第二版jsonp

--- handwriting-series/JsonP.js
+++ handwriting-series/JsonP.js
@@ -52,3 +52,80 @@ function jsonp({ url, data }, options = { callbackName: 'callback' }) {
 
     jsonp({url: 'http://localhost:3000', data: {name: 'tom', age: 18}})
     .then(res => console.log(res))
+
+
+// 第二版 
+
+let count = 1
+
+function createURL(url, data = {}, cb, id) {
+  url += (~url.indexOf('?') ? '&' : '?')
+  const queryStr = Object.keys(data).map(key => {
+    return `${key}=${data[key]}`
+  }).join('&')
+  url += queryStr
+  url += `&${cb}=${id}`
+  url = url.replace('?&', '?')
+  url = url.replace('&&', '&')
+  return url
+}
+
+function noop() { }
+
+function jsonP(url, data, opts) {
+  // 1、校验参数，返回值设置
+  // 2、url生成
+  // 3、全局回调设置
+  // 4、回收清理
+  // 5、异常处理
+  // 6、拓展超时时间
+  return new Promise((resolve, reject) => {
+    if (!url || typeof url !== 'string') {
+      return reject('url 参数不合法')
+    }
+    if (!data || typeof data !== 'object') {
+      return reject('data 参数不合法')
+    }
+    if (!opts) opts = {}
+    const prefix = opts.prefix || '__Exe'
+    const cbName = opts.name
+    const id = prefix + (count++)
+    const timeout = opts.timeout || 1000 * 6
+    const container = document.getElementsByTagName('script')[0] || document.head
+    let script = null
+    let timer = null
+
+    if (timeout) {
+      timer = setTimeout(() => {
+        cleanup()
+      }, timeout)
+    }
+
+    function cleanup() {
+      if (script.parentNode) script.parentNode.removeChild(script)
+      window[id] = noop
+      if (timer) clearTimeout(timer)
+    }
+
+    window[id] = function(data) {
+      if (data) {
+        resolve(data)
+      } else {
+        reject()
+      }
+      cleanup()
+    }
+
+    script = document.createElement('script')
+    script.src = createURL(url, data, cbName, id)
+    script.onerror = function(e) {
+      reject(e)
+      cleanup()
+    }
+    container.appendChild(script)
+  })
+}
+
+jsonP('http://localhost:3000', { name: 'tom', age: 18 }, { name: 'callback' })
+  .then(res => console.log(res))
+
