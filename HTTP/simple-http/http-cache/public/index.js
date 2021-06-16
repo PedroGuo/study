49095824ab3e5114930ffeaf700b869c48ba6cc2
boy-yan <1950304028@qq.con>
2021-06-16 18:06:56 +0800

    feat: 图片上传(60%)

--- HTTP/simple-http/http-cache/public/index.js
+++ HTTP/simple-http/http-cache/public/index.js
@@ -1,74 +1,112 @@
+axios.defaults.baseURL = 'http://localhost:3000/';
+function getEle(select) {
+    return document.querySelector(select)
+}
 
-// var vm = new Vue({
-//     data: {
-//         message: 'Hello Vue!'
-//       },
-//     // 选项
-//     method: {
-//         handleClickParent() {
-//             console.log(this);
-//         }
-//     }
-// })
-var app = new Vue({
-    el: '#app',
-    data: {
-        images: [],
-        dragover: false,
-        disabled: false,
-        message: 'Hello Vue!'
-    },
-    methods: {
-        handleClick() {
-            if (!this.disabled) {
-                this.$refs.input.value = null;
-                this.$refs.input.click();
-            }
-        },
-        handleKeydown(e) {
-            if (e.target !== e.currentTarget) return;
-            if (e.keyCode === 13 || e.keyCode === 32) {
-                this.handleClick();
-            }
-        },
-        handleInputChange(e) {
-            console.log(e.target.files);
-        },
-        onDragover() {
-            if (!this.disabled) {
-                this.dragover = true;
-            }
-        },
-        onDrop(e) {
-            if (this.disabled || !this.uploader) return;
-            const accept = this.uploader.accept;
-            this.dragover = false;
-            if (!accept) {
-                this.$emit('file', e.dataTransfer.files);
-                return;
-            }
-            this.$emit('file', [].slice.call(e.dataTransfer.files).filter(file => {
-                const { type, name } = file;
-                const extension = name.indexOf('.') > -1
-                    ? `.${name.split('.').pop()}`
-                    : '';
-                const baseType = type.replace(/\/.*$/, '');
-                return accept.split(',')
-                    .map(type => type.trim())
-                    .filter(type => type)
-                    .some(acceptedType => {
-                        if (/\..+$/.test(acceptedType)) {
-                            return extension === acceptedType;
-                        }
-                        if (/\/\*$/.test(acceptedType)) {
-                            return baseType === acceptedType.replace(/\/\*$/, '');
-                        }
-                        if (/^[^\/]+\/[^\/]+$/.test(acceptedType)) {
-                            return type === acceptedType;
-                        }
-                        return false;
-                    });
-            }));
-        }
+function addEvent(dom, eventType, handle) {
+    if (!dom) return
+    dom.addEventListener(eventType, handle)
+    return () => {
+        dom.removeEventListener(eventType, handle)
     }
-})
+}
+
+
+
+
+function onProgress(progressEvent) {
+    let complete = (progressEvent.loaded / progressEvent.total * 100 | 0) + '%'
+    const uploadMessage = '上传 ' + complete
+    console.log(uploadMessage);
+}
+
+function uploadFile(file) {
+    const formData = new FormData()
+    formData.append('file', file)
+    axios.post('/upload/single', formData, {
+        onUploadProgress(progressEvent) {
+            if (progressEvent.lengthComputable) {
+                onProgress(progressEvent);
+            }
+        }
+    })
+}
+
+function checkFileType() {
+
+}
+
+
+
+function getDataURL(file) {
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader()
+        fileReader.readAsDataURL(file)
+        fileReader.onload = function () {
+            resolve(fileReader.result)
+        }
+        fileReader.onerror = function () {
+            reject()
+        }
+    })
+
+}
+
+function renderImageList(list) {
+    if (!list || !Array.isArray(list)) return
+    const uploadList = getEle('.el-upload-list')
+    uploadList.innerHTML = null
+    const Fragment = document.createDocumentFragment()
+    list.forEach((item) => {
+        const li = document.createElement('li')
+        const image = document.createElement('img')
+        image.src = item.src
+        li.appendChild(image)
+        Fragment.appendChild(li)
+    })
+    uploadList.appendChild(Fragment)
+}
+
+
+function main() {
+    const uploadContainer = getEle('.el-upload-dragger')
+    const inputElement = getEle('.el-upload__input')
+    const previewImageList = []
+
+    async function previewFile(e) {
+        let fileList = e.target.files || e.dataTransfer.files
+        if (!fileList || !fileList.length) return
+        fileList = [...fileList]
+        for (const file of fileList) {
+            uploadFile(file)
+            const dataURL = await getDataURL(file)
+            previewImageList.push({
+                src: dataURL
+            })
+        }
+        renderImageList(previewImageList)
+    }
+
+
+    addEvent(uploadContainer, 'click', function (e) {
+        inputElement.value = null;
+        inputElement.click()
+    })
+
+    addEvent(uploadContainer, 'dragover', function (e) {
+        e.preventDefault()
+    })
+
+    addEvent(uploadContainer, 'drop', async function (e) {
+        e.preventDefault()
+        previewFile(e)
+    })
+
+    addEvent(inputElement, 'change', async function (e) {
+        previewFile(e)
+    })
+
+}
+
+main()
+
